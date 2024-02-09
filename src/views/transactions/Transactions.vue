1d3ea897bb5a8d07a477b1db105e182e348745aa
neiltonrodriguez <neiltonrodriguez@gmail.com>
2024-02-09 19:11:54 -0300

    Ajustes gerais no CRUD de transactions

--- src/views/transactions/Transactions.vue
+++ src/views/transactions/Transactions.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="mt-3 max-w-7xl focus-visible: rounded-xl m-auto w-full h-full">
     <div class="flex">
-      <div
-        class="flex-1 p-3 flex flex-col items-center justify-center bg-white"
-      >
+      <div class="flex-1 p-3 flex flex-col items-center justify-center bg-white">
         <div class="text-slate-400">
           Saldo total:
           <template v-if="balanceTotal > 0">
@@ -16,15 +14,11 @@
       </div>
     </div>
     <div class="flex gap-2 mt-3">
-      <div
-        class="flex-1 p-3 flex flex-col items-center justify-center bg-white"
-      >
+      <div class="flex-1 p-3 flex flex-col items-center justify-center bg-white">
         <div class="text-slate-400">últimas entradas</div>
         <div class="text-4xl text-green-600">R${{ transactionsIn }}</div>
       </div>
-      <div
-        class="flex-1 p-3 flex flex-col items-center justify-center bg-white"
-      >
+      <div class="flex-1 p-3 flex flex-col items-center justify-center bg-white">
         <div class="text-slate-400">últimas saídas</div>
         <div class="text-4xl text-red-600">R${{ transactionsOut }}</div>
       </div>
@@ -42,49 +36,22 @@
             </tr>
           </thead>
           <tbody>
-            <tr
-              v-for="t in transactions"
-              :key="t.id"
-              class="odd:bg-white even:bg-gray-50 border-b"
-            >
-              <td
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-              >
+            <tr v-for="t in transactions" :key="t.id" class="odd:bg-white even:bg-gray-50 border-b">
+              <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                 {{ t.title }}
               </td>
               <td class="px-6 py-4">{{ t.category }}</td>
               <td class="px-6 py-4 flex gap-3">
                 <template v-if="t.type_transaction == 'gasto'">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="w-5 h-5 text-red-600"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
-                    />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-5 h-5 text-red-600">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
                   </svg>
                 </template>
                 <template v-else>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="w-5 h-5 text-green-600"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
-                    />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-5 h-5 text-green-600">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
                   </svg>
                 </template>
                 {{ t.type_transaction }}
@@ -97,19 +64,13 @@
       </div>
     </div>
     <div class="flex items-end justify-end gap-3 p-3">
-      <button
-        @click="showDrawer"
-        class="bg-teal-600 items-end p-3 rounded-md text-white hover:bg-teal-700"
-      >
+      <button @click="showDrawer" class="bg-teal-600 items-end p-3 rounded-md text-white hover:bg-teal-700">
         Nova transação
       </button>
     </div>
   </div>
 
-  <div
-    v-if="isVisibleDrawer"
-    class="left-0 top-0 w-full fixed h-screen"
-  >
+  <div v-if="isVisibleDrawer" class="left-0 top-0 w-full fixed h-screen">
     <!-- drawer component -->
     <Drawer @fechar="showDrawer" @payload="createTransactions" />
     <div class="w-full h-screen bg-black opacity-70 " @click="showDrawer"></div>
@@ -151,7 +112,8 @@ function createTransactions(payload) {
       showDrawer();
     })
     .catch((e) => {
-      swal("Erro!", "Email ou senha incorreta", "error");
+      console.log(e)
+      swal("Erro!", e.message, "error");
     });
 }
 
@@ -162,7 +124,7 @@ function getTransactions() {
       transactions.value = res.data.data;
     })
     .catch((e) => {
-      swal("Erro!", "Email ou senha incorreta", "error");
+      swal("Erro!", e.message, "error");
     });
 }
 
@@ -173,7 +135,7 @@ function getBalanceTotal() {
       balanceTotal.value = res.data.data;
     })
     .catch((e) => {
-      swal("Erro!", "Email ou senha incorreta", "error");
+      swal("Erro!", e.message, "error");
     });
 }
 
@@ -185,7 +147,7 @@ function getTransactionsTotal() {
       transactionsOut.value = res.data.data.total_out;
     })
     .catch((e) => {
-      swal("Erro!", "Email ou senha incorreta", "error");
+      swal("Erro!", e.message, "error");
     });
 }
 
